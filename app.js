c6c3b1464d8b5025ab67af9cf822dec0980c98c4
Edvinest <66338497+Edvinest@users.noreply.github.com>
2023-04-20 15:44:59 +0300

    added style to grid

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 window.onload = function(){
-    const grid = document.querySelector('.grid')
+    const grid = document.querySelector('.grid');
     drawGrid(grid, 16);
 }
 
 function drawGrid (container, size){
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
             container.appendChild(document.createElement('div'))
-        }
     }
     return container;
 }
