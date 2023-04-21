fe122d64c9d807d4d0d093f933c59605c6090874
Edvinest <66338497+Edvinest@users.noreply.github.com>
2023-04-21 18:15:41 +0300

    added color picker

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const grid = document.querySelector('.grid');
 const uiContainer = document.querySelector('.ui')
 const resizeBtn = document.createElement('button');
+const colorPicker = document.createElement('input')
+colorPicker.setAttribute('type', 'color')
 resizeBtn.textContent = 'Resize'
 resizeBtn.addEventListener('click', changeSize)
 
-uiContainer.appendChild(resizeBtn)
-const gridSize = changeSize;
-drawGrid(grid, gridSize);
+uiContainer.append(resizeBtn, colorPicker)
+drawGrid(grid, 16);
 
 function drawGrid(container, size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -22,18 +23,44 @@ function drawGrid(container, size) {
 
 function draw() {
     const pixels = document.querySelectorAll('.grid div');
+    const pencilColor = (color) => {
+        return (e) => {
+            e.target.style.backgroundColor = color;
+        }
+    };
+    let isDrawing = false;
 
     pixels.forEach(pixel => {
-        pixel.addEventListener('mouseleave', (e) => {
-            e.target.style.backgroundColor = 'black';
+        pixel.addEventListener('mousedown', () => {
+            isDrawing = true;
+        })
+
+        pixel.addEventListener('mouseup', () => {
+            isDrawing = false;
+        })
+
+        pixel.addEventListener('click', (e) => {
+            pencilColor(colorPicker.value)(e)
+        })
+
+        pixel.addEventListener('mousemove', (e) => {
+            if (isDrawing) {
+                pencilColor(colorPicker.value)(e);
+            }
         })
     })
 }
 
-function changeSize(){
-    let newSize = prompt("What size would you like the grid to be? (1-100)", 16);
+function changeSize() {
+    let newSize = parseInt(prompt("What size would you like the grid to be? (1-100)", 16));
     if (newSize > 100 || newSize == null) newSize = 16
-    return newSize;
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+
+    drawGrid(grid, newSize);
+    draw()
 }
 
 draw();
